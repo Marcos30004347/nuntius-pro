14cf549e3f6f75326f5aa292a6bb794dce3fa6bb
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2022-09-22 22:29:59 -0300

    chore: adds pre-commit lint husky

--- nuntius-pro-app/src/App.js
+++ nuntius-pro-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <GlobalStyles />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+            <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
