56c132b1f938a4e682a321c3721acbb7c6d2f991
Vinicius Julião R <40807325+vrjuliao@users.noreply.github.com>
2022-10-09 20:14:16 -0300

    Setup/socket.io (#12)
    Co-authored-by: luiz-couto <luiz.felipe.556@outlook.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,26 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 import routes from "./routes.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
+const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+io.on("connection", (socket) => {
+  console.log("user connected");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
