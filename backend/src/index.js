861a7bfd8bbfce7e5b65684554edb6bef283a1b6
vrjuliao <vrjuliao@hotmail.com>
2022-10-09 19:56:01 -0300

    socket.io startup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,32 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io"
 
 import routes from "./routes.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+const io = new Server(server);
 
+const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+io.on('connection', (socket) => {
+  console.log("alo")
+  console.log(socket);
 });
 
-app.listen(port, () => {
+// socket.on('greeting-from-server', function (message) {
+//   document.body.appendChild(
+//       document.createTextNode(message.greeting)
+//   );
+
+server.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
--- nuntius-pro-app/src/App.js
+++ nuntius-pro-app/src/App.js
@@ -1,7 +1,15 @@
 import { GlobalStyles } from './GlobalStyles';
 import { NavGlobal } from './design-system/components/Navigation';
 
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:8000');
+console.log(socket);
+
 function App() {
+  // socket.emit('greeting-from-client', {
+  //   greeting: 'Hello Server'
+  // })
   return (
     <>
       <GlobalStyles />
