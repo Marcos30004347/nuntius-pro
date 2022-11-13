e8d60ac0a0d5cee7ecce93757c7182126692164c
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2022-11-13 00:38:22 -0300

    feat: adds avatar and name

--- nuntius-pro-app/src/domains/profile/ui/screens/edit/EditProfile.js
+++ nuntius-pro-app/src/domains/profile/ui/screens/edit/EditProfile.js
@@ -1,3 +1,25 @@
+import { Avatar } from '../../../../../design-system/components/Avatar';
+import { Button } from '../../../../../design-system/components/Button';
+import { Typography } from '../../../../../design-system/components/Typography';
+import { PrivatePage } from '../../../../../shared/ui/components/PrivatePage';
+import { Container, FistSection } from './EditProfile.styles';
+
 export const EditProfile = () => {
-  return <div> oi </div>;
+  // get from context
+  const name = 'Aline';
+  return (
+    <PrivatePage>
+      <Container>
+        <Avatar size="large" />
+        <FistSection>
+          <Typography variant="paragraphRegular">@{name}</Typography>
+          <div>
+            <Button size="small" variant="tertiary">
+              Editar perfil
+            </Button>
+          </div>
+        </FistSection>
+      </Container>
+    </PrivatePage>
+  );
 };
