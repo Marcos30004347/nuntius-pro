cd4f73e2bca5ad2cd78444dbe4e544222b9f8ad5
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2022-11-13 01:57:34 -0300

    refactor: removes unnecessary ...props

--- nuntius-pro-app/src/design-system/components/Avatar/Avatar.js
+++ nuntius-pro-app/src/design-system/components/Avatar/Avatar.js
@@ -3,14 +3,14 @@ import { Container, Image, Fallback } from './Avatar.styles';
 import { FallbackUser } from './FallbackUser';
 
 // TODO: add context to get user pallete color to FallbackUser
-export const Avatar = ({ src, size = 'medium', onClick, ...props }) => {
+export const Avatar = ({ src, size = 'medium', onClick }) => {
   return (
     <Container $sizing={size} onClick={onClick}>
       {src ? (
-        <Image src={src} alt="User's profile" {...props} />
+        <Image src={src} alt="User's profile" />
       ) : (
         <Fallback>
-          <FallbackUser color="#27c241" size={size} {...props} />
+          <FallbackUser color="#27c241" size={size} />
         </Fallback>
       )}
     </Container>
