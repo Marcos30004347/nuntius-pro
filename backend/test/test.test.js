2838abff9c8f477d89f8103530583975f46f8384
wesleyraul <42242383+wesleyraul@users.noreply.github.com>
2022-11-29 15:17:21 -0300

    Add jest (#43)

---
+++ backend/test/test.test.js
@@ -0,0 +1,10 @@
+import { signin } from "../src/services/auth"
+
+describe('test jest', ()=>{
+
+    test('test', ()=>{
+        const result = true;
+        expect(result).toBe(true);
+    })
+})
+ 
